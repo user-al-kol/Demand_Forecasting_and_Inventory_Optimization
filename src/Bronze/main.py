dbd105147e3381b8020f2d39ad37a876a8a18302
alkol <algitkol@gmail.com>
2026-04-03 18:34:25 +0300

    Start developing the bronze layer code.

--- src/Bronze/main.py
+++ src/Bronze/main.py
@@ -1,5 +1,7 @@
 import os
-import logging 
+import logging
+from datetime import datetime 
+from urllib.parse import unquote
 from pyspark.sql import SparkSession
 
 logging.basicConfig(
@@ -10,7 +12,76 @@ logging.basicConfig(
 if __name__ == '__main__':
 
     INVENTORY_MOVEMENT_SOURCE = os.environ.get("IM_SOURCE_DIR")
+    LOGICAL_DATE = os.environ.get("LOGICAL_DATE")
     inventory_movement_files = os.listdir(INVENTORY_MOVEMENT_SOURCE)
+    now = datetime.now()
+    present_date = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    spark = SparkSession.builder.appName("UpsertBronzeData").getOrCreate()
+    logging.info("Spark Session successfully started.")
 
     logging.info("The inventory movement files are: ")
     logging.info(inventory_movement_files)
+
+    for file in inventory_movement_files:
+
+        if 'ingestion_date' in file:
+
+            #logging.debug(f"This file is: {file}")
+            file_date = file.split("=")[1]
+            logging.info(f"The date of the file: {file_date}")
+
+            decoded = unquote(file_date)
+            dt = datetime.strptime(decoded, "%Y-%m-%d %H:%M:%S")
+            iso_file_date = dt.isoformat()
+
+            if iso_file_date >= LOGICAL_DATE:
+
+                logging.info(f"That's your new file: {file}")
+                logging.info(f"ISO: {iso_file_date}")
+                logging.info(f"Logical Date: {LOGICAL_DATE}")
+
+                full_file_path = os.path.join(INVENTORY_MOVEMENT_SOURCE,file)
+                logging.info(f"Full file path: {full_file_path}")
+
+                new_inventory_movements_df = spark.read.parquet(full_file_path)
+
+                new_inventory_movements_df.createOrReplaceTempView("inventory")
+                
+                new_inventory_movements_view = spark.sql(
+                    f"""
+                        SELECT
+                            *, 
+                            TIMESTAMP('{present_date}') AS processed_date
+                        FROM
+                            inventory
+                    """
+                )
+
+                new_inventory_movements_view.select(["movement_id","movement_ts","movement_date","movement_type"]).show(5)
+                new_inventory_movements_view.select(["product_id","sku","location_id","location_code"]).show(5)
+                new_inventory_movements_view.select(["qty_delta","ref_order_id","ref_order_type","notes"]).show(5)
+                new_inventory_movements_view.select(["erp_export_ts","processed_date"]).show(5)
+                new_inventory_movements_view.printSchema()
+
+                # new_inventory_movements_view = spark.sql(
+                #     f"""
+                #         WITH ranked AS (
+                #             SELECT *,
+                #                 TIMESTAMP('{present_date}') AS processed_date,
+                #                 ROW_NUMBER() OVER (
+                #                     PARTITION BY movement_id, movement_date
+                #                     ORDER BY movement_id
+                #                 ) AS rn
+                #             FROM 
+                #                 inventory
+                #         )
+                #         SELECT 
+                #             *
+                #         FROM 
+                #             ranked
+                #         WHERE 
+                #             rn = 1
+                # """)
+
+    spark.stop()
