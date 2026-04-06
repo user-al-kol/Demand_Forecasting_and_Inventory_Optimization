268529183af4be0c8cfa64cb12e138c0f6284049
alkol <algitkol@gmail.com>
2026-04-06 15:10:17 +0300

    Add changes to main.py in order to combine all the actions of the bronze layer.

--- src/Ingestion/main.py
+++ src/Ingestion/main.py
@@ -1,29 +1,50 @@
 import os
 import logging
+from datetime import datetime
 from pyspark.sql import SparkSession
-from ingestion import get_files, make_partition
+from common.config import *
+from common.utils import get_logger, get_todays_files, divide_files, find_latest_file
+from common.spark_utils import partition, transform_inventory
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-)
 
 if __name__ == "__main__":
 
-    SOURCE_DIR = os.environ.get("SOURCE_DIR")
+    logger = get_logger(logging.INFO)
 
-    logging.info("File ingestion process is starting.")
+    present_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Bronze Layer
+    # Ingestion process
+    logger.info("File ingestion process is starting.")
 
     spark = SparkSession.builder.appName("PartitionFiles").getOrCreate()
-    logging.info("Spark session started.")
+    logger.info("Spark session started.")
+
     # Call ingestor
-    todays_files = get_files(SOURCE_DIR)
+    todays_files = get_todays_files(SOURCE_DIR,LOGICAL_DATE,logger)
 
-    logging.info("Files to be ingested:")
-    logging.info(todays_files)
+    logger.info("Files to be ingested:")
+    logger.info(todays_files)
+    # Divide the files
 
-    make_partition(SOURCE_DIR,todays_files,spark)
+    inventory_movement_file, sales_file = divide_files(todays_files)
+
+    partition(SOURCE_DIR, IM_DESTINATION_DIR, inventory_movement_file, spark, logger)
+    partition(SOURCE_DIR, S_DESTINATION_DIR, sales_file, spark, logger)
+
+    logger.info("Files ingested successfully.")
+
+    # Save files in delta tables.
+    inventory_movement_file_partitioned = find_latest_file(IM_SOURCE_DIR, LOGICAL_DATE, logger)
+    sales_file_partitioned = find_latest_file(IM_SOURCE_DIR, LOGICAL_DATE, logger)
+
+    # Define the schema #schema
+
+    inventory_movement_file_partitioned_df = spark.read.parquet(inventory_movement_file_partitioned)
+
+
+    transform_inventory(inventory_movement_file_partitioned_df, present_date, spark)
 
     spark.stop()
 
-    logging.info("Files ingested successfully.")
+    
